1ee07a735b21f9f0239525a96b7859e7c46a61db
GFernande7 <gikon145@yandex.ru>
2024-06-09 17:49:03 +0300

    added slug

--- app/apps/categories/models.py
+++ app/apps/categories/models.py
@@ -1,15 +1,20 @@
+import uuid
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
+from django.urls import reverse
 
-
-class Category(MPTTModel):
-    id = models.IntegerField(primary_key=True, unique=True)
+class Category(MPTTModel, models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
     parent = TreeForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name="children")
 
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+            return reverse('dead-shop:product_list_by_category', args=[self.slug])
+    
     class MPTTMeta:
         order_insertion_by = ["name"]
     
