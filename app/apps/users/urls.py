710a13a6522642ca78be1b7fd55a82de6ab91dd0
GFernande7 <gikon145@yandex.ru>
2024-07-02 21:18:41 +0300

    reworked user authorization and base template

--- app/apps/users/urls.py
+++ app/apps/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UserCreateView, logout_view, sign_in
+from .views import UserCreateView, logout_view, sign_in, index
 
 app_name = "users"
 
 urlpatterns = [
+    path('', view=index, name='index'),
     path("sign-up/", view=UserCreateView.as_view(), name="sign_up"),
     path("sign-in/", view=sign_in, name="sign_in"),
     path("sign-out/", view=logout_view, name="sign_out"),
--- app/apps/users/views.py
+++ app/apps/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
+from django.http import JsonResponse
 from django.views.generic import CreateView
 
 from .forms import LoginForm, UserCreateForm
@@ -9,9 +10,18 @@ from .models import Client
 
 class UserCreateView(CreateView):
     form_class = UserCreateForm
-    template_name = "users/sign-up.html"
-    success_url = reverse_lazy("users:sign_in")
+    success_url = reverse_lazy("products:index")
     model = Client
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(email=email, password=password)
+        login(self.request, user)
+        return JsonResponse({"success": True})
+
+    def form_invalid(self, form):
+        return JsonResponse({"success": False, "errors": form.errors})
 
 
 def sign_in(request):
@@ -23,16 +33,21 @@ def sign_in(request):
             user = authenticate(request, email=email, password=password)
             if user:
                 login(request, user)
-                print("success")
-                return redirect("products:index")
+                return JsonResponse({"success": True})
             else:
-                print("error")
-                return render(request, "users/login.html")
+                return JsonResponse({"success": False, "error": "Invalid email or password"})
+        else:
+            return JsonResponse({"success": False, "errors": form.errors})
     else:
         form = LoginForm()
-    return render(request, "users/login.html", {"form": form})
+    return JsonResponse({"success": False, "error": "Invalid request method"})
 
 
 def logout_view(request):
     logout(request)
     return redirect("products:index")
+
+def index(request):
+    login_form = LoginForm()
+    register_form = UserCreateForm()
+    return render(request, 'base.html', {'login_form': login_form, 'register_form': register_form})
